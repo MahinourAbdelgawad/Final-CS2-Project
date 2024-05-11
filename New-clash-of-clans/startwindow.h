63a74d87783e4286448ef577fc285f3175a4893d
Mahinour Abdelgawad <MahinourAbdelgawad@aucegypt.edu>
2024-05-11 21:58:46 +0300

    final final

---
+++ New-clash-of-clans/startwindow.h
@@ -0,0 +1,28 @@
+#ifndef STARTWINDOW_H
+#define STARTWINDOW_H
+
+#include "game.h"
+#include <QDialog>
+
+namespace Ui {
+class startwindow;
+}
+
+class startwindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit startwindow(QWidget *parent = nullptr);
+    ~startwindow();
+    void updateProgressBar();
+private slots:
+
+
+
+
+private:
+    Ui::startwindow *ui;
+};
+
+#endif // STARTWINDOW_H
